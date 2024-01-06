42992a2d52b6a9bb4430f2da4b12e32c06cdcb2a
dilara-b <152744629+dilara-b@users.noreply.github.com>
2024-01-06 17:17:47 +0100

    Update Data collection.R
    added comments for clarity

--- Data collection.R
+++ Data collection.R
@@ -51,12 +51,12 @@ correct1 = function(subject,number){
 }
 
 #returns fraction of correct answers for test 5
-#emotional face-in-the-crowd test consisting of trials with 9 different human faces, either with one emotional facial expression (the target; either happy or angry) among 8 neutral facest (the distractors) or with 9 neutral faces without any target
+#emotional face-in-the-crowd test consisting of trials with 9 different human faces, either with one emotional facial expression (the target: either happy or angry) among 8 neutral faces (the distractors) or with 9 neutral faces without any target
 correct5 = function(subject,number){
   
   df = subject[[number]]
 
-  #calculate mean/sd response times for responses where candidate pressed either right or left to identify outliers, using right and wrong answers          
+  #calculate mean/sd response times for responses where candidate pressed either right or left to identify outliers, using correct and incorrect answers          
   response_time_mean = mean(df$response_time[df$response == "right" | df$response == "left"])
   response_time_sd = sd(df$response_time[df$response == "right" | df$response == "left"])
   
